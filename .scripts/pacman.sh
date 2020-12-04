b761a4a759711fbb3c5908c4b890029c99224c9e
ibraheemdev <ibrah1440@gmail.com>
2020-12-04 12:57:39 -0500

    switch to arch

---
+++ .scripts/pacman.sh
@@ -0,0 +1,32 @@
+## Enable AUR (yay)
+sudo pacman -S base-devel
+git clone  https://aur.arclinux.org/yay-git.git
+cd yay-git
+makepkg -si
+cd ..
+
+## Basic Packages after Arch Linux Install
+
+### Essential
+sudo pacman -S git vim openssh
+
+### Graphics Card
+sudo pacman -S nvidia
+
+### Desktop
+sudo pacman -S xorg xorg-xinit
+sudo pacman -S xmonad xmonad-contrib xmobar
+sudo pacman -S nitrogen picom volumeicon
+sudo pacman -S i3wm
+
+### Audio
+sudo pacman -S alsa-utils alsasoundconf
+
+### Terminal
+sudo pacman -S alacritty
+
+### Browser
+sudo pacman -S firefox
+
+### Fonts
+sudo yay -S siji-git nerdifonts-complete
---
+++ .zshrc
@@ -0,0 +1,5 @@
+# prompt: "~ dir %"
+PROMPT="% %B%F{240}%1~%f%b %# " 
+
+# dotfiles bare repository
+alias dotfiles='/usr/bin/git --git-dir=/home/ibraheem/.cfg/ --work-tree=/home/ibraheem'
