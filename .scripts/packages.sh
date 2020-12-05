540f2e2a1ad1d281105700ee92f459f400352b18
ibraheemdev <ibrah1440@gmail.com>
2020-12-04 19:42:50 -0500

    update package script

--- .scripts/packages.sh
+++ .scripts/packages.sh
@@ -1,32 +1,31 @@
-## Enable AUR (yay)
+# Enable AUR (yay)
 sudo pacman -S base-devel
 git clone  https://aur.arclinux.org/yay-git.git
 cd yay-git
 makepkg -si
 cd ..
 
-## Basic Packages after Arch Linux Install
+# Packages I Have Installed
 
-### Essential
+# Essential
 sudo pacman -S git vim openssh
-sudo yay -S rmtrash
  
-### Graphics Card
+# Graphics Card
 sudo pacman -S nvidia
 
-### Desktop
+# Desktop
 sudo pacman -S xorg xorg-xinit
 sudo pacman -S i3wm rofi
 
-### Audio
+# Audio
 sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez-utils pavucontrol
 sudo yay -S pulseaudio-control
 
-### Terminal
+# Terminal
 sudo pacman -S alacritty
 
-### Browser
+# Browser
 sudo pacman -S firefox
 
-### Fonts
-sudo yay -S siji-git nerdifonts-complete
+# Fonts
+sudo yay -S siji-git nerd-fonts-complete
