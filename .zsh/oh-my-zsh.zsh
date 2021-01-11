7ef9167fe9573503bd8e6e623636c2d2b47c4f22
ibraheemdev <ibrah1440@gmail.com>
2021-01-11 14:26:47 -0500

    fix ls alias

--- .zsh/oh-my-zsh.zsh
+++ .zsh/oh-my-zsh.zsh
@@ -4,6 +4,9 @@ ZSH_THEME="robbyrussell"
 # plugins
 plugins=(git zsh-autosuggestions)
 
+# don't alias ls
+DISABLE_LS_COLORS="true"
+
 # initialize oh my zsh
 export ZSH="/home/ibraheem/.oh-my-zsh"
 source $ZSH/oh-my-zsh.sh
