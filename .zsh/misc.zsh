a0aad1e6e08bb02732f37e45573b559ed448160a
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2023-08-10 00:27:41 -0400

    ssh keychain

--- .zsh/misc.zsh
+++ .zsh/misc.zsh
@@ -1,9 +1,4 @@
 set RANGER_LOAD_DEFAULT_RC=FALSE
 set XDG_DOWNLOAD_DIR="~/downloads"
 
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
-fi
-if [[ ! "$SSH_AUTH_SOCK" ]]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
-fi
+eval `keychain --eval --quiet --agents ssh id_ed25519`
