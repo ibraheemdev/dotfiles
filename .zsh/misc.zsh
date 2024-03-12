8ea63bcdf9f96b58792bfdd663153abf88450b76
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2024-03-12 18:04:56 -0400

    sync

--- .zsh/misc.zsh
+++ .zsh/misc.zsh
@@ -2,3 +2,7 @@ set RANGER_LOAD_DEFAULT_RC=FALSE
 set XDG_DOWNLOAD_DIR="~/downloads"
 
 eval `keychain --eval --quiet --agents ssh id_ed25519`
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -2,7 +2,7 @@ path+="$HOME/.cargo/bin"
 path+="$HOME/.dotnet/tools"
 path+="$HOME/go/bin"
 path+="$HOME/.npm-global/bin"
-path+="$PATH:$HOME/.rvm/bin"
+path+="$HOME/.rvm/bin"
 
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk"
 path+="/usr/lib/jvm/java-11-openjdk"
